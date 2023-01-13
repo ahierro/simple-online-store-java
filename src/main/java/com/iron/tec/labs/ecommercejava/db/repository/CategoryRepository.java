9d29c3189b185f1d4b3a13e06745d92201ff387d
Ale <alejandrohierro@gmail.com>
2023-01-13 16:27:23 -0300

    refactor: change interface for repositories

--- src/main/java/com/iron/tec/labs/ecommercejava/db/repository/CategoryRepository.java
+++ src/main/java/com/iron/tec/labs/ecommercejava/db/repository/CategoryRepository.java
@@ -4,7 +4,7 @@ import com.iron.tec.labs.ecommercejava.db.entities.Category;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -12,7 +12,7 @@ import reactor.core.publisher.Mono;
 import java.util.UUID;
 
 @Repository
-public interface CategoryRepository extends ReactiveCrudRepository<Category, UUID> {
+public interface CategoryRepository extends R2dbcRepository<Category, UUID> {
 
     @Modifying
     @Query("delete from category where id = :id")
--- src/main/java/com/iron/tec/labs/ecommercejava/db/repository/ProductRepository.java
+++ src/main/java/com/iron/tec/labs/ecommercejava/db/repository/ProductRepository.java
@@ -4,7 +4,7 @@ import com.iron.tec.labs.ecommercejava.db.entities.Product;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -12,7 +12,7 @@ import reactor.core.publisher.Mono;
 import java.util.UUID;
 
 @Repository
-public interface ProductRepository extends ReactiveCrudRepository<Product, UUID> {
+public interface ProductRepository extends R2dbcRepository<Product, UUID> {
 
     @Modifying
     @Query("delete from product where id = :id")
