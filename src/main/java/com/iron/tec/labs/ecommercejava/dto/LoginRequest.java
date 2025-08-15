be1f9005e4dc7a669fc040b2d46e00fe1b4b79d7
Alejandro Hierro <alejandrohierro@gmail.com>
2025-08-15 17:10:15 -0300

    feat(config): add Liquibase configuration variables and update defaults
    Add Swagger example to LoginRequest

--- src/main/java/com/iron/tec/labs/ecommercejava/dto/LoginRequest.java
+++ src/main/java/com/iron/tec/labs/ecommercejava/dto/LoginRequest.java
@@ -1,9 +1,13 @@
 package com.iron.tec.labs.ecommercejava.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
-public record LoginRequest(@NotEmpty @Length(min = 4, max = 50) String username,
-                           @NotEmpty @Length(min = 5, max = 50) String password) {
+public record LoginRequest(
+        @Schema(description = "Username of the user", example = "admin")
+        @NotEmpty @Length(min = 4, max = 50) String username,
+        @Schema(description = "Password of the user", example = "p4ssw0rd")
+        @NotEmpty @Length(min = 5, max = 50) String password) {
 
 }
